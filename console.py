ff3e48b2644e341e939f22172eb2cff23676f605
Chad96 <chadrackndalamba@gmail.com>
2024-02-12 20:46:56 +0200

    task

--- console.py
+++ console.py
@@ -11,6 +11,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 from models import storage
+from models import classes
 import shlex
 import re
 
